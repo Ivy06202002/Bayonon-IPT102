143600ddb5b60696c8e58998e68139b316974b8b
Ivy06202002 <bayonon.ivy.06202002@gmail.com>
2023-09-23 13:23:02 +0800

    Updated info

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var studentInfoArray = new[]
+        {
+            new StudentInfoModel { Name = "Ivy Bayonon", StudentId = "S001", StudentAge = "21" },
+            new StudentInfoModel { Name = "Summer Bayonon", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Kate Bayonon", StudentId = "S003", StudentAge = "31" }
+        };
+
+        return View(studentInfoArray);
+    }
+}
